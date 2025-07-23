6e38b62c35ad2ed7553d0c6e0f473552867b356d
Yeghishe <yeghishehak08@gmail.com>
2025-07-23 14:54:47 -0700

    giving opacity 0 for better UI

--- client/SeatCraft/src/pages/Store/Store.jsx
+++ client/SeatCraft/src/pages/Store/Store.jsx
@@ -55,22 +55,22 @@ function Store({style}) {
 
             <div className={styles.items}>
                 <div ref={imagesRef} className={styles.itemsImageContainer}>
-                    <Link to="/product/regal-lounger">
+                    <Link className={styles.imagesLink} to="/product/regal-lounger">
                         <img draggable="false" src="/store/Item1.png" alt="Image 1" className={`${styles.itemImage} ${styles.itemImage1}`} />
                     </Link>
-                    <Link to="/product/auracurve">
+                    <Link className={styles.imagesLink} to="/product/auracurve">
                         <img draggable="false" src="/store/Item2.png" alt="Image 2" className={`${styles.itemImage} ${styles.itemImage2}`} />
                     </Link>
-                    <Link to="/product/miraform">
+                    <Link className={styles.imagesLink} to="/product/miraform">
                         <img draggable="false" src="/store/Item3.png" alt="Image 3" className={`${styles.itemImage} ${styles.itemImage3}`} />
                     </Link>
-                    <Link to="/product/modulux">
+                    <Link className={styles.imagesLink} to="/product/modulux">
                         <img draggable="false" src="/store/Item4.png" alt="Image 4" className={`${styles.itemImage} ${styles.itemImage4}`} />
                     </Link>
-                    <Link to="/product/velvet-crest">
+                    <Link className={styles.imagesLink} to="/product/velvet-crest">
                         <img draggable="false" src="/store/Item5.png" alt="Image 5" className={`${styles.itemImage} ${styles.itemImage5}`} />
                     </Link>
-                    <Link to="/product/vivoseat">
+                    <Link className={styles.imagesLink} to="/product/vivoseat">
                         <img draggable="false" src="/store/Item6.png" alt="Image 6" className={`${styles.itemImage} ${styles.itemImage6}`} />
                     </Link>
                 </div>
